39893bc900e0a49013e6cd5894e8732efa1e6799
igh_life <164069169+17Midas@users.noreply.github.com>
2025-11-16 23:08:29 +0300

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
-import { createContext } from 'react';
+import React from 'react';
 
-const UserContext = createContext(null);
+// Create the context with a default value of null
+const UserContext = React.createContext(null);
 
+// Export it for use in other components
 export default UserContext;
