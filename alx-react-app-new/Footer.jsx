5827ca54a266b0d79fdca2758f533d9f284dc8fb
igh_life <164069169+17Midas@users.noreply.github.com>
2025-11-16 22:42:21 +0300

    Create Footer.jsx

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: "30px",
+        padding: "15px",
+        textAlign: "center",
+        backgroundColor: "#222",
+        color: "white",
+        borderRadius: "6px"
+      }}
+    >
+      <p>© 2025 My Travel Blog</p>
+    </footer>
+  );
+}
+
+export default Footer;
