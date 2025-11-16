235f7ca9f58fa6288ea11f8ac11a0751465bc150
igh_life <164069169+17Midas@users.noreply.github.com>
2025-11-16 20:33:43 +0300

    Create EditRecipeForm.jsx

---
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useRecipeStore from "../stores/recipeStore";
+
+const EditRecipeForm = ({ recipe }) => {
+  const updateRecipe = useRecipeStore((state) => state.updateRecipe);
+  const [title, setTitle] = useState(recipe.title);
+  const [description, setDescription] = useState(recipe.description);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateRecipe({
+      id: recipe.id,
+      title,
+      description,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ marginBottom: 20 }}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br /><br />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <br /><br />
+      <button type="submit">Save Changes</button>
+    </form>
+  );
+};
+
+export default EditRecipeForm;
