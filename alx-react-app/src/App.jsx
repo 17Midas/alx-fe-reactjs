edc774e5f391b63ed48b386210c63c69e480696a
igh_life <164069169+17Midas@users.noreply.github.com>
2025-11-16 22:04:33 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <h1>Vite + React</h1>
-      <h2>Hello ALX!</h2>
-      <button onClick={() => setCount(count + 1)}>
-        Count is {count}
-      </button>
-    </div>
-  )
+    <>
+      <WelcomeMessage />
+    </>
+  );
 }
 
-export default App
+export default App;
